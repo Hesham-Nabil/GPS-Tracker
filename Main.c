91bc7b14a1f613240813f1b4b8cb74bdcb712600
Yahiabob <167820892+Yahiabob@users.noreply.github.com>
2024-04-23 19:03:02 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,1 +1,15 @@
+#include "tm4c123gh6pm.h"
+#include "UART.h"
+#include "GPS_Receiver.c"
 
+int main(void)
+{
+    UART_Init();
+    while (1){
+
+        while (!check_logname()); //wait for desired log name
+
+
+    }
+    return 0;
+}
