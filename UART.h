eacf5b979e935865ec006b71724466d389c07b74
Mohamed-elmoner <146053482+Mohamed-elmoner@users.noreply.github.com>
2024-04-23 19:54:25 +0200

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,6 +1,9 @@
 #ifndef UART
 #define UART
-void UART_Init(void);
-bool UART0_data_available(void);
-char UART0_read_data(void);
+void UART0_Init();
+void UART1_Init();
+bool UART1_data_available();
+char UART1_read_data();
+bool UART0_data_available();
+char UART0_read_data();
 #endif
