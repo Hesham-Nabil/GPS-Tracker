ba954f2f04aaf8b54b33c115c374edef94912091
Yahiabob <167820892+Yahiabob@users.noreply.github.com>
2024-04-23 19:34:17 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,13 +2,15 @@
 #include "UART.h"
 #include "GPS_Receiver.c"
 
+char GPS_data[80];
+
 int main(void)
 {
     UART_Init();
     while (1){
 
         while (!check_logname()); //wait for desired log name
-
+        getGpsData();
 
     }
     return 0;
