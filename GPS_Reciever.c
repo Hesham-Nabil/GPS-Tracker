dd884dfe6b64e969c4d476a54cc699a766520af1
Yahiabob <167820892+Yahiabob@users.noreply.github.com>
2024-04-23 19:01:36 +0200

    Update GPS_Reciever.c

--- GPS_Reciever.c
+++ GPS_Reciever.c
@@ -1,1 +1,14 @@
+#include "UART.h"
 
+char check_logname(){
+    char i=0;
+    char[] logname = "$GPRMC,";
+    while (i<7){
+        if (logname[i] != UART0_read_data()){
+            i=0;
+            break;
+        }
+        i++;
+    }
+    return i;
+}
