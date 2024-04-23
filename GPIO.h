f57be1c73161262cf5b65d3ffc22b638cc2adfc4
Hesham Nabil <91266833+Hesham-Nabil@users.noreply.github.com>
2024-04-23 18:43:07 +0200

    Add files via upload

---
+++ GPIO.h
@@ -0,0 +1,10 @@
+
+void GPIO_PortF_Init();
+void GPIO_PortE_Init();
+void LED_init();
+void Switches_init();
+void LED_ON();
+void LED_OFF();
+unsigned char SW1_Input();
+unsigned char SW2_Input();
+
