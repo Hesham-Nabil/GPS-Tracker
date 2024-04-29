6ff81c562d933b89fc04867801b53d3dd38eaf30
hesham <heshamnabil1@hotmail.com>
2024-04-30 01:26:27 +0300

    edit

--- src/Main.c
+++ src/Main.c
@@ -7,7 +7,30 @@ char GPS_data[80];
 int main(void)
 { while(1)
 {
+int main(void)
+{
+    GPIO_PortF_Init();
+    UART0_PORTA_Init();
+    UART1_PORTB_Init();
+    Switches_init();
+    LED_init();
+    //double coordinates[300][2];       //this line should be written in main before GPS_Start() should be called
+    //double Distance=0;                //this line should be written in main before GPS_Start() should be called
+    //int i = 0;    
 
+    char buffer[500]={};                //initialize a buffer with size very very big to be certain that the data is recieved 
+    char test;
+    while (1)
+    {   
+    
+    if ( UART1_RECIEVE_CHAR( &test))
+    {
+        for (int i = 0; i < 1000; i++);
+        UART0_TRANSMIT_CHAR(test);
+    }
+    }
+    return 0;
+}
 }
     return 0;
 }
