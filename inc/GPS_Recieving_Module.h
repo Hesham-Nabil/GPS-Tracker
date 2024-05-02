77facf114fb76f96ce3efd7554c2b4f62c5b8538
Mohamed Ahmed Mohamed Hussein <160266120+MohamedHussein27@users.noreply.github.com>
2024-05-02 03:29:41 +0300

    header file for new GPS receiving module

---
+++ inc/GPS_Recieving_Module.h
@@ -0,0 +1,2 @@
+#define MAX_ROWS 12  // Maximum number of rows in the 2-D array (in the desired data from the GPS)
+#define MAX_COLS 12  // Maximum number of columns in the 2-D array (in the desired data from the GPS)
