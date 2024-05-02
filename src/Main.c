f26d4c21e418828ce248a00a5c5848c7acd3ce7f
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-05-02 19:36:44 +0300

    Edit

--- src/Main.c
+++ src/Main.c
@@ -1,23 +1,20 @@
-#include "PortsDef.h"
+#include "tm4c123gh6pm.h"
 #include "UART.h"
 #include "GPS_Reciever.h"
-
-
-char GPS_data[80];
+#include "GPIO.h"
 int main(void)
 {
     GPIO_PortF_Init();
     UART0_PORTA_Init();
     UART1_PORTB_Init();
     LED_init();
-    double coordinates[300][2] = {};                                //this line should be written in main before GPS_Start() should be called
-    double Distance = 0;                                            //this line should be written in main before GPS_Start() should be called
-    int i = 0;
-    char buffer[10] = {};                                           //initialize a buffer with size very very big to be certain that the data is recieved
+    Switches_init();
     while (1)
     {
-        GPS_Start(&Distance, coordinates, &buffer, i);
-
-
+        if (SW1_Input() == 1)
+        {
+            LED_ON();
+        }
+        LED_OFF();
     }
 }
