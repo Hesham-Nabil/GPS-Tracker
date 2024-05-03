57d632a1c3e7830b61c5506749cd8a68e877700d
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-05-03 03:06:13 +0300

    edit

--- src/Main.c
+++ src/Main.c
@@ -12,14 +12,14 @@ int main(void)
     Switches_init();
     SysTick_Init();
     const int length = 300;
-    char buffer[length];
-    while (1)
-    { UART0_RECIEVE_DATA(&buffer,length);
-      delay(20);
+    char buffer[length];         
+    while (1)                   
+    { UART0_RECIEVE_DATA(buffer,length);
+      delay(200);
       for(int i =0 ; i<length;i++){
       UART0_TRANSMIT_CHAR(buffer[i]);
       }
-     memset(buffer,0,length);
+    memset(buffer,0,length);
     
     }
 }
