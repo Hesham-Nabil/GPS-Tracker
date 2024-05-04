227deb851ce40bbc6ec69923570647ab05e9d4f4
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-05-04 23:23:05 +0300

    edits

--- src/GPS_Reciever.c
+++ src/GPS_Reciever.c
@@ -18,33 +18,42 @@ const double Earth_Radius = 6371000;
 
 void GPS_Start(double *Distance, double coordinates[300][2], char* buffer, int i){                      // A function that parces the data after $GPRMC, and retrieve the data in variables
     while (1)
-    {         
+    {   const int length = 300   ;   
         const char *RMC = "$GPRMC";                                                                     // match string
-        const char *RMC_PTR = NULL;                                                                     // "TO BE" address of the $ sign of the GPRMC
-        UART1_RECIEVE_DATA(&buffer,300);                                                                // this function fills the buffer filled with the data and the garbage
-        RMC_PTR = strstr( buffer , RMC);                                                                // search for a needle ("$GPRMC") in a hay stack (buffer)
+        const char *RMC_PTR = NULL;   
+        UART0_TRANSMIT_CHAR('2');                                                                  // "TO BE" address of the $ sign of the GPRMC
+        UART1_RECIEVE_DATA(buffer,length); 
+        UART0_TRANSMIT_CHAR('3');                                                               // this function fills the buffer filled with the data and the garbage
+        RMC_PTR = strstr( buffer , RMC); 
+        UART0_TRANSMIT_CHAR('4');                                // search for a needle ("$GPRMC") in a hay stack (buffer)
         if (RMC_PTR == NULL)
         continue;
         double time, latitude, longitude, knots;                                                        // the variables that we want from the gps
         char status, ns, ew;
         delay(10);
-        //sscanf takes string input ($ bta3 al GPRMC) and format string ("$GPRMC,%f,%c,%f,%c,%f,%c,%f,") and refrenced var
+        UART0_TRANSMIT_CHAR('5');//sscanf takes string input ($ bta3 al GPRMC) and format string ("$GPRMC,%f,%c,%f,%c,%f,%c,%f,") and refrenced var
         if (sscanf(RMC_PTR, "$GPRMC,%lf,%c,%lf,%c,%lf,%c", &time, &status, &latitude, &ns , &longitude , &ew) == 6 )
-        {  
+        {  UART0_TRANSMIT_CHAR('6');
             if (status != 'A')
             continue;
+            UART0_TRANSMIT_CHAR('7');
             coordinates[i][0]=longitude;                                                                //longitude should be stored in the 1st column
             coordinates[i][1]=latitude;                                                                 //lattitude should be stored in the 2nd column
             i++;
+
         }
         else{
             continue;
         }
-        if (i == 300)                                                                                   //if i becomes too large exit the inf loop
-        break;
+        if (i == 3) {
+UART0_TRANSMIT_CHAR('x');
+         break;
+        }                                                                                  //if i becomes too large exit the inf loop
+        
 
         if (i==1)   
             continue;
+            UART0_TRANSMIT_CHAR('8');
             double long1_rad = Torad(Todegree(coordinates[i-1][0]));
             double lat1_rad  = Torad(Todegree(coordinates[i-1][1]));
             double long2_rad = Torad(Todegree(coordinates[i-2][0]));
--- src/Main.c
+++ src/Main.c
@@ -3,6 +3,7 @@
 #include "GPS_Reciever.h"
 #include "GPIO.h"
 #include "Systick.h"
+
 int main(void)
 {
     GPIO_PortF_Init();
@@ -12,15 +13,44 @@ int main(void)
     Switches_init();
     SysTick_Init();
     const int length = 300;
-    char buffer[length];         
+    char buffer[length]; 
+    const int L = 3;
+    const int W = 2;
+    double coordinates[L][W];
+    double distance =0;
+    int i = 0 ;         
     while (1)                   
-    { UART0_RECIEVE_DATA(buffer,length);
-      delay(200);
-      GPS_Start()
-    //   for(int i =0 ; i<length;i++){
-    //   UART0_TRANSMIT_CHAR(buffer[i]);
-    //   }
-    //   UART0_TRANSMIT_CHAR('\n');
-    
-    }
+    { UART0_TRANSMIT_CHAR('1');
+GPS_Start(&distance,coordinates,buffer,i);
+    UART0_TRANSMIT_CHAR('c');
+    char output_buffer[10];
+    UART0_TRANSMIT_CHAR('d');
+UART0_TRANSMIT_CHAR('\n');
+UART0_TRANSMIT_CHAR((((int) coordinates[0][0]/1000) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) coordinates[0][0]/100) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) coordinates[0][0]/10) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) coordinates[0][0]/1) % 10) + 48);
+UART0_TRANSMIT_CHAR('.');
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][0]*10)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][0]*100)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][0]*1000)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][0]*10000)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][0]*100000)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][0]*1000000)) % 10) + 48);
+UART0_TRANSMIT_CHAR(' ');
+UART0_TRANSMIT_CHAR((((int) coordinates[0][1]/1000) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) coordinates[0][1]/100) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) coordinates[0][1]/10) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) coordinates[0][1]/1) % 10) + 48);
+UART0_TRANSMIT_CHAR('.');
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][1]*10)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][1]*100)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][1]*1000)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][1]*10000)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][1]*100000)) % 10) + 48);
+UART0_TRANSMIT_CHAR((((int) (coordinates[0][1]*1000000)) % 10) + 48);
+
+UART0_TRANSMIT_CHAR('\n');
+}
+
 }
