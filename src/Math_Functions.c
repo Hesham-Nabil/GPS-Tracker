bc8da6f6cf6b15ecb518f184f48b1c12ac8660c4
Abdelrahman Khaled <167885587+B0dyaa@users.noreply.github.com>
2024-05-08 23:20:53 +0300

    Update Math_Functions.c

--- src/Math_Functions.c
+++ src/Math_Functions.c
@@ -1,14 +1,6 @@
 #include <math.h>
 const float PI = 3.14159;
-
-double Todegree(double angle)
-{
-	int degree = (int)angle / 100;
-	double minutes = angle - (double)degree * 100;
-	return (degree + (minutes / 60));
-}
-
-double Torad(double angle)
+double Torad(double angle) 
 {
 	return (angle * PI / 180);
 }
