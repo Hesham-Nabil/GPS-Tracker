138086f0a71c0f8f274af7a56534dbfdfe34f73b
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-05-12 21:23:58 +0300

    edits

--- src/Main.c
+++ src/Main.c
@@ -52,12 +52,12 @@ int main(void)
    char buffer[length];
    char LCD_output_buffer[16] = {};
    char output_buffer[16] = {};
-
+ int Mem_Address;
+ int Mem_Block;
    int gps_loop_counter = 0;
    int flag = 0;
    while (1)
    {
-      
       LED_OFF();
       LED_RED_ON();
       if (SW1_Input() == 1)
@@ -65,8 +65,8 @@ int main(void)
          flag = 1;
          LED_OFF();
          LED_Green_ON();
-         int Mem_Address = 0;
-         int Mem_Block = 0;
+          Mem_Address = 0;
+          Mem_Block = 0;
       }
       while (flag)
       {
@@ -81,25 +81,59 @@ int main(void)
          ///////////////Saving Distance/////////////////
          LCD_1602_I2C_Write("Saving..  ");
          delay(100);
-         if (Mem_Address < 16)
+         if (Mem_Address < 16) //& SW2_Input() == 1
          {
+            if (Mem_Block == 31 && (Mem_Address == 14))
+            {
+               flag = 0;
+               break;
+            }
             EepromWrite(coordinates[0][0] * 100000, Mem_Address, Mem_Block);
+            EepromWrite(Mem_Address, 14, 31);
             EepromWrite(coordinates[0][1] * 100000, ++Mem_Address, Mem_Block);
+            EepromWrite(Mem_Address, 14, 31);
             EepromWrite(coordinates[1][0] * 100000, ++Mem_Address, Mem_Block);
+            EepromWrite(Mem_Address, 14, 31);
             EepromWrite(coordinates[1][1] * 100000, ++Mem_Address, Mem_Block);
+            EepromWrite(Mem_Address, 14, 31);
             Mem_Address++;
          }
          else if (Mem_Block < 32)
          {
             Mem_Block++;
             Mem_Address = 0;
+            EepromWrite(Mem_Block, 15, 31);
          }
          else
-            break;
+         {  int max_block=EepromRead(15,31);
+            int max_address=EepromRead(14,31);
+            double memRead = 0;
+            int fr_part = 0;
+            int int_part = 0;
+            for (int j; j < max_block; j++)
+            {
+               for (int i = 0; i < max_address; i++)
+               {
+                  memRead = EepromRead(i, j) / 100000.0;
+                  int_part = ((int)memRead);
+                  fr_part = (memRead - (int)memRead) * 100000;
+                  sprintf(output_buffer, "(%d.%d,", int_part, fr_part);
+                  UART0_TRANSMIT_DATA(output_buffer, 9);
+                  memRead = EepromRead(++i, j) / 100000.0;
+                  int_part = ((int)memRead);
+                  fr_part = (memRead - (int)memRead) * 100000;
+                  sprintf(output_buffer, "-%d.%d)", int_part, fr_part);
+                  UART0_TRANSMIT_DATA(output_buffer, 10);
+                  UART0_TRANSMIT_CHAR('\n');
+
+                  flag = 0;
+                  break;
+               }
+            }
+         }
       }
    }
 }
-
 // double memRead = 0;
 //          int fr_part = 0;
 //          int int_part = 0;
