4758af87117e774ca2892838e5daa1d5bc300264
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-05-12 13:19:07 +0300

    edit

--- src/draft.c
+++ src/draft.c
@@ -184,3 +184,128 @@
 //         i++;
 //     }
 // }
+#include "tm4c123gh6pm.h"
+#include "UART.h"
+#include "GPS_Reciever.h"
+#include "GPIO.h"
+#include "Systick.h"
+#include "EEPROM.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "LCD.h"
+#include "i2c.h"
+
+int main(void)
+{
+   SysTick_Init();
+   GPIO_PortF_Init();
+   UART0_PORTA_Init();
+   UART1_PORTB_Init();
+   EepromInit();
+   LED_init();
+   Switches_init();
+   I2C_Init();
+   LCD_1602_I2C_Init();
+   const int length = 300;
+   double coordinates[3][2];
+   double distance = 0;
+   char buffer[length];
+   char LCD_output_buffer[16] = {};
+   char output_buffer[16] = {};
+   int fr_part = 0;
+   int int_part = 0;
+   int gps_loop_counter = 0;
+   int address = 0;
+   int block = 0;
+   int flag = 0;
+   double rec = 0;
+   char x;
+   while (1)
+   {
+      //    UART1_RECIEVE_CHAR(&x);
+      //    UART0_TRANSMIT_CHAR(x);
+      // }
+      EepromWrite(30.167437 * 100000, address, block);
+      rec = EepromRead(address, block) / 100000.0;
+      int_part = ((int)rec);
+      fr_part = (rec - (int)rec) * 100000;
+      sprintf(output_buffer, "(%d.%d,", int_part, fr_part);
+      UART0_TRANSMIT_DATA(output_buffer, 9);
+      EepromWrite(31.164723 * 100000, ++address, block);
+      int_part = ((int)rec);
+      fr_part = (rec - (int)rec) * 100000;
+      sprintf(output_buffer, "-%d.%d)", int_part, fr_part);
+      UART0_TRANSMIT_DATA(output_buffer, 10);
+   }
+}
+
+//      EepromWrite(30.167437 * 100000, address, block);
+//        rec = EepromRead(address, block) / 100000.0;
+//         int_part = ((int)rec);
+//       fr_part = (rec - (int)rec) * 100000;
+//        sprintf(output_buffer, "(%d.%d,", int_part, fr_part);
+//        UART0_TRANSMIT_DATA(LCD_output_buffer,9);
+//        EepromWrite(31.164723 * 100000, ++address, block);
+//        int_part = ((int)rec);
+//       fr_part = (rec - (int)rec) * 100000;
+//       sprintf(output_buffer, "-%d.%d)", int_part, fr_part);
+//        UART0_TRANSMIT_DATA(LCD_output_buffer,10);
+
+// LED_OFF();
+// LED_RED_ON();
+// if (SW1_Input() == 1)
+// {
+//    flag = 1;
+// }
+// while (flag)
+// {
+//    LED_OFF();
+//    LED_Green_ON();
+//    GPS_Start(&distance, coordinates, buffer, gps_loop_counter);
+//    /////////////Displaying Distance///////////////
+//    LCD_1602_I2C_Write("Calculating..  ");
+//    delay(100);
+//    LCD_DISPLAY_FLOAT(distance);
+//    delay(100);
+
+//    ///////////////Saving Distance/////////////////
+//    LCD_1602_I2C_Write("Saving..  ");
+//    delay(100);
+//    if (address < 16)
+//    {
+//       EepromWrite(30.167437 * 100000, address, block);
+//       EepromWrite(31.164723 * 100000, ++address, block);
+//       EepromWrite(coordinates[1][0] * 100000, ++address, block);
+//       EepromWrite(coordinates[1][1] * 100000, ++address, block);
+//       address++;
+//    }
+//    else if (block < 32)
+//    {
+//       block++;
+//       address = 0;
+//    }
+//    else if(SW2_Input == 1)
+//    // break;
+//    // if (SW2_Input == 1)
+//    {
+//       for (int j; j < block; j++)
+//       {
+//          for (int i = 0; i < address; i++)
+//          {
+//             rec = EepromRead(i, j) / 100000.0;
+//             int_part = ((int)rec);
+//             fr_part = (rec - (int)rec) * 1000000;
+//             sprintf(output_buffer, "(%d.%d,", int_part, fr_part);
+//             UART0_TRANSMIT_DATA(LCD_output_buffer,9);
+//             rec = EepromRead(++i, j) / 100000.0;
+//             int_part = ((int)rec);
+//             fr_part = (rec - (int)rec) * 100000;
+//             sprintf(output_buffer, "-%d.%d)", int_part, fr_part);
+//             UART0_TRANSMIT_DATA(LCD_output_buffer,10);
+//             UART0_TRANSMIT_CHAR('\n');
+//          }
+//       }
+//       flag = 0;
+//    }
+// }
