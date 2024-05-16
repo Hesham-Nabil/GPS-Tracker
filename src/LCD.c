e5f9463db0e4b074ec52a8a4f81aa7d55dbd0790
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-05-16 22:33:44 +0300

    edit

--- src/LCD.c
+++ src/LCD.c
@@ -35,30 +35,30 @@ void LCD_1602_I2C_Send_Data(char byte)
 
 void LCD_1602_I2C_Init()
 {
-    LCD_1602_I2C_Send_Command(0x03); // Initialize in 8-bit mode
-    LCD_1602_I2C_Send_Command(0x03); // Initialize in 8-bit mode
-    LCD_1602_I2C_Send_Command(0x03); // Initialize in 8-bit mode
-    LCD_1602_I2C_Send_Command(0x02); // Initialize in 4-bit mode
-    LCD_1602_I2C_Send_Command(0x20); // set to 28 to enable multiline
-    LCD_1602_I2C_Send_Command(0x00); // Display off
-    LCD_1602_I2C_Send_Command(0x08); // Display off
-    LCD_1602_I2C_Send_Command(0x00); // Clear display
-    LCD_1602_I2C_Send_Command(0x01); // Clear display
-    LCD_1602_I2C_Send_Command(0x06); // Entry mode set: Increment cursor, No display shift
-    LCD_1602_I2C_Send_Command(0x0E); // Display on, Cursor off, Blink off
+    LCD_1602_I2C_Send_Command(0x03);                                                // Initialize in 8-bit mode
+    LCD_1602_I2C_Send_Command(0x03);                                                // Initialize in 8-bit mode
+    LCD_1602_I2C_Send_Command(0x03);                                                // Initialize in 8-bit mode
+    LCD_1602_I2C_Send_Command(0x02);                                                // Initialize in 4-bit mode
+    LCD_1602_I2C_Send_Command(0x20);                                                // set to 28 to enable multiline
+    LCD_1602_I2C_Send_Command(0x00);                                                // Display off
+    LCD_1602_I2C_Send_Command(0x08);                                                // Display off
+    LCD_1602_I2C_Send_Command(0x00);                                                // Clear display
+    LCD_1602_I2C_Send_Command(0x01);                                                // Clear display
+    LCD_1602_I2C_Send_Command(0x06);                                                // Entry mode set: Increment cursor, No display shift
+    LCD_1602_I2C_Send_Command(0x0E);                                                // Display on, Cursor off, Blink off
 }
 
 void LCD_1602_I2C_Write(char *ptr)
 {
 
-    LCD_1602_I2C_Send_Command(0x01); // Clear display
+    LCD_1602_I2C_Send_Command(0x01);                                                // Clear display
 
     while (*ptr)
     {
         LCD_1602_I2C_Send_Data(*ptr++);
     }
 
-    LCD_1602_I2C_Send_Command(0x10); // Moves Cursor
+    LCD_1602_I2C_Send_Command(0x10);                                                // Moves Cursor
     I2C0_Write_Single(LCD_1602_7_Bit_Address, 1 << 3);
 }
 
--- src/i2c.c
+++ src/i2c.c
@@ -5,21 +5,21 @@
 void I2C_Init()
 {
     SYSCTL_RCGCI2C_R = SYSCTL_RCGCI2C_R0;
-    SYSCTL_RCGCGPIO_R |= 0x2;     // Enable clock to GPIOB
-    GPIO_PORTB_ODR_R |= (1 << 3); // Ensure I2C SDA pin is configured open drain
+    SYSCTL_RCGCGPIO_R |= 0x2;                                                       // Enable clock to GPIOB
+    GPIO_PORTB_ODR_R |= (1 << 3);                                                   // Ensure I2C SDA pin is configured open drain
     GPIO_PORTB_AFSEL_R |= (1 << 3) | 1 << 2;
     GPIO_PORTB_PUR_R |= (1 << 3) | 1 << 2;
     GPIO_PORTB_DEN_R |= (1 << 3) | 1 << 2;
-    I2C0_MCR_R = 0x10; // Enable Master function mode
-    I2C0_MTPR_R = 0x7; // Clock divider, SCL_PERIOD = 2 × (1 + TPR) × (SCL_LP + SCL_HP) × CLK_PRD
-                       // 2 × (1 + I2CMTPR) × (6 + 4) × 1/(SYSCLK)
+    I2C0_MCR_R = 0x10;                                                              // Enable Master function mode
+    I2C0_MTPR_R = 0x7;                                                              // Clock divider, SCL_PERIOD = 2 × (1 + TPR) × (SCL_LP + SCL_HP) × CLK_PRD
+                                                                                    // 2 × (1 + I2CMTPR) × (6 + 4) × 1/(SYSCLK)
 }
 
 void I2C0_Write_Single(int slave_address, char data)
 {
     I2C0_MSA_R = slave_address << 1;
     I2C0_MDR_R = data;
-    I2C0_MCS_R = 7; // RUN, START, STOP if bytes_count
+    I2C0_MCS_R = 7;                                                                 // RUN, START, STOP if bytes_count
     while (I2C0_MCS_R & 1)
         ;
 }
